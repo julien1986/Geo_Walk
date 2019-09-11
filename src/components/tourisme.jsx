51b4fe9103d1b699d30707bc5693a48937d0b58e
ingrwf <ingrwf@cpg-453069.cepegra2008.lan>
2019-09-11 16:04:45 +0200

    ultimate last final

--- src/components/tourisme.jsx
+++ src/components/tourisme.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {} from "semantic-ui-react";
 
 //IMPORT CONPONENTS
-import List_parcours from "./toursime/list-parcours";
-import ScanQr from "./toursime/scanQR";
+import Tourisme_container from "./toursime/tourisme_container";
 
 //créer le context pour la requete axios vers la liste des parcours
---
+++ src/components/toursime/tourisme_container.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../scss/tourisme/tourisme_container.scss";
+
+//IMPORT COMPONENTS
+import List_parcours from "./toursime/list-parcours";
+import ScanQr from "./toursime/scanQR";
