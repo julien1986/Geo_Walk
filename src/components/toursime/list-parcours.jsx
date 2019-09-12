c32a7c93a9bd2aed738b5bd7a8241f7acee74913
ingrwf <ingrwf@cpg-453070.cepegra2008.lan>
2019-09-12 16:32:32 +0200

    12/09

--- src/components/toursime/list-parcours.jsx
+++ src/components/toursime/list-parcours.jsx
@@ -1,15 +1,62 @@
-import React from "react";
-import { Container, Segment } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Container, Segment, List } from "semantic-ui-react";
+import axios from "axios";
+import uid from "uid";
 
 //IMPORT CONPONENTS
 import Plan from "./plan";
 
 export default function ListParcours() {
+
+
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:8080/trips')
+    .then(response => {
+      console.log('Data: ', response.data);
+      setTrips(response.data)
+    })
+    .catch(error => {
+      console.log('An error occurred:', error);
+    });
+  }, [])
+
+
+  const handleList = () =>{
+
+    let localTrips;
+    if(localStorage){
+      localTrips = JSON.parse(localStorage.getItem("trips"));
+      console.log(localTrips)
+      setTrips(localTrips)
+
+      return (
+      trips.map(trip =>
+      <List.Item key={uid()}>{trip.trip_name}, {trip.categorie}</List.Item>
+      ))
+    }
+    else{
+      return "Vous n'avez pas encore enregistré de parcours"
+    };
+
+    // let temp = [];
+    // trips.map(t => temp = [...temp, {
+    //     "trip_name": t.trip_name,
+    //     "categorie": t.categorie,
+    //     "pois": t.pois
+    //   }]);
+    // //console.log(temp)
+    // localStorage.setItem("trips", JSON.stringify(temp))
+  }
+
   return (
     <>
       <h1>Je suis le module de liste des parcours</h1>
       <Segment>
-        <Plan />
+      <Plan />
+          <List>{handleList}</List>
       </Segment>
     </>
   );
