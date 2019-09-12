33f2c74c5b57b115cabd5714c374c5327b64de52
ingrwf <ingrwf@cpg-453071.cepegra2008.lan>
2019-09-12 14:51:43 +0200

    first qr commit

--- src/components/toursime/scanQR.jsx
+++ src/components/toursime/scanQR.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {} from "semantic-ui-react";
+import QrReader from "react-qr-reader";
+import axios from "axios";
 
 //IMPORT CONPONENTS
-import Plan from "./plan";
-import Popup from "./popup";
+
+//import Popup from "./popup";
 
 export default function ScanQR() {
-  return <h1>Je suis le module de scan</h1>;
+  const [trips, setTrips] = useState([]);
+  const [getQR, setQR] = useState({ resultat: "" });
+
+  useEffect(() => {
+    axios
+      .get("http://10.1.107.4:8080/trips")
+      .then(response => {
+        // Handle success.
+        console.log("Data: ", response.data);
+        setTrips(response.data);
+      })
+      .catch(error => {
+        // Handle error.
+        console.log("An error occurred:", error);
+      });
+  }, []);
+  const handleError = () => {
+    alert("Code non valide");
+  };
+  const handleScan = data => {
+    if (data) {
+      console.log(data);
+      setQR({ resultat: data });
+    }
+  };
+  return (
+    <div className="scanQR">
+      <h2>Scannez un QR Code</h2>
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
 }
