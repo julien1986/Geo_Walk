709267a6d8c90e250690635a6a6949ac482ad9a6
ingrwf <ingrwf@cpg-453071.cepegra2008.lan>
2019-09-13 09:34:59 +0200

    vdd test1

--- src/components/toursime/scanQR.jsx
+++ src/components/toursime/scanQR.jsx
@@ -13,33 +13,55 @@ export default function ScanQR() {
   const [getQR, setQR] = useState({ resultat: "" });
   //const { newtrips, setnewtrips } = useContext(DataContext);
 
-  useEffect(() => {
-    axios
-      .get("http://10.1.107.4:8080/trips")
-      .then(response => {
-        // Handle success.
-        console.log("Data: ", response.data);
-        setTrips(response.data);
-      })
-      .catch(error => {
-        // Handle error.
-        console.log("An error occurred:", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("10.1.107.4:8080/trips")
+  //     .then(response => {
+  //       // Handle success.
+  //       console.log("Data: ", response.data);
+  //       setTrips(response.data);
+  //     })
+  //     .catch(error => {
+  //       // Handle error.
+  //       console.log("An error occurred:", error);
+  //     });
+  // }, []);
   const handleError = () => {
     alert("Code non valide");
   };
   const handleScan = data => {
     if (data) {
       console.log(data);
-      trips.map(trip => {
-        if (data === trip.id) {
-          console.log("OK");
-        }
-      });
+      setQR({ resultat: data });
+      axios
+        .get(`10.1.107.4:8080/trips/${getQR.resultat}`)
+        .then(response => {
+          // Handle success.
+          console.log("Data: ", response.data);
+          let temp = [];
+          trips.map(
+            t =>
+              (temp = [
+                ...temp,
+                {
+                  trip_name: t.trip_name,
+                  categorie: t.categorie,
+                  pois: t.pois
+                }
+              ])
+          );
+          //console.log(temp)
+          localStorage.setItem("trips", JSON.stringify(temp));
+        })
+        .catch(error => {
+          // Handle error.
+          console.log("An error occurred:", error);
+        });
+
       alert("Le QR Code a bien été scanné");
     }
   };
+
   return (
     <div className="scanQR">
       <h2>Scannez un QR Code</h2>
